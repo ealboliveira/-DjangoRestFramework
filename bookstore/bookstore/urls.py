1f1f26f41bcaf6fbcfc9453cf078a86776f0cad4
Eduardo Alberto de Oliveira <138824845+ealboliveira@users.noreply.github.com>
2024-03-27 16:04:40 -0300

    Update urls.py

--- bookstore/bookstore/urls.py
+++ bookstore/bookstore/urls.py
@@ -1,8 +1,14 @@
+import debug_toolbar
+from bookstore import views
 from django.contrib import admin
-from django.urls import path
-from .views import update_server_view  # Importe a função de visualização necessária
+from django.urls import include, path, re_path
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('update_server/', update_server_view, name='update_server'),  # Nova rota
-]
+    path("__debug__/", include(debug_toolbar.urls)),
+    path("admin/", admin.site.urls),
+    re_path("bookstore/(?P<version>(v1|v2))/", include("order.urls")),
+    re_path("bookstore/(?P<version>(v1|v2))/", include("product.urls")),
+    path("api-token-auth/", obtain_auth_token, name="api_token_auth"),
+    path("update_server/", views.update, name="update"),
+    path("hello/", views.hello_world, name="hello_world"),
