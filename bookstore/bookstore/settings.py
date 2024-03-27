fc20c39a7c3f9b7a4af3262e529a8f250aa9638b
Eduardo Alberto de Oliveira <138824845+ealboliveira@users.noreply.github.com>
2024-03-27 10:43:06 -0300

    Update settings.py

--- bookstore/bookstore/settings.py
+++ bookstore/bookstore/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-dy@2r%y7tszk#cm53b$&470#(kfj%6gdmzo#t_kz(@r_%yf!$(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'ealboliveira.pythonanywhere.com']
 
 
 # Application definition
