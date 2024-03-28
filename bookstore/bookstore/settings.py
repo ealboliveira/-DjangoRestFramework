17dd61d0a2fd926e74c25a665aa8d393e44205f4
Eduardo Alberto de Oliveira <138824845+ealboliveira@users.noreply.github.com>
2024-03-27 23:56:08 -0300

    Update settings.py

--- bookstore/bookstore/settings.py
+++ bookstore/bookstore/settings.py
@@ -27,9 +27,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "order",
     "product",
-    'rest_framework',
+    
 ]
 
 
