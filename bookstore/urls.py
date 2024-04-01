04934c60c9faa93bc00d0750a8a9caed541c9eda
Eduardo Oliveira <ealb.oliveira@gmail.com>
2024-04-01 17:46:52 -0300

    Commit suas alterações locais

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -1,14 +1,13 @@
-import debug_toolbar
-from bookstore import views
-from django.contrib import admin
 from django.urls import include, path, re_path
+from django.contrib import admin
 from rest_framework.authtoken.views import obtain_auth_token
+from . import views
 
 urlpatterns = [
     path("__debug__/", include(debug_toolbar.urls)),
     path("admin/", admin.site.urls),
-    re_path("bookstore/(?P<version>(v1|v2))/", include("order.urls")),
-    re_path("bookstore/(?P<version>(v1|v2))/", include("product.urls")),
+    re_path("bookstore/order/(?P<version>(v1|v2))/", include("order.urls")),
+    re_path("bookstore/product/(?P<version>(v1|v2))/", include("product.urls")),
     path("api-token-auth/", obtain_auth_token, name="api_token_auth"),
     path("update_server/", views.update, name="update"),
     path("hello/", views.hello_world, name="hello_world"),
