741df4e2070563d229aeec732340ab26696ba3a7
Eduardo Oliveira <ealb.oliveira@gmail.com>
2024-04-01 17:32:15 -0300

    arrumando urls

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -12,3 +12,4 @@ urlpatterns = [
     path("api-token-auth/", obtain_auth_token, name="api_token_auth"),
     path("update_server/", views.update, name="update"),
     path("hello/", views.hello_world, name="hello_world"),
+]
